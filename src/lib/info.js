9d4c9a95298008b5a29ecb1e3d8418102b40350e
Amr Badawy <46964544+kashik0i@users.noreply.github.com>
2022-09-30 15:35:12 +0200

    added personal urls

--- src/lib/info.js
+++ src/lib/info.js
@@ -5,9 +5,44 @@
 /**
  * Used for the header of the page and in SEO
  */
-export const name = 'SvelteKit Blog Template' // replace with your name
+export const name = 'Garbage Collector'
 
 /**
  * Used for SEO in /posts/[slug]
  */
-export const website = 'https://yourdomain.com'
+export const website = 'https://blog.badawy.dev'
+
+/**
+ * Some Links
+ */
+export const links = {
+  bio: {
+    personal_site: 'https://badawy.dev/',
+    github: 'https://github.com/kashik0i',
+    youtube: 'https://www.youtube.com/channel/UCKJYG18tmKTRLoU6flxJa_Q',
+    twitch: 'https://www.twitch.tv/amrbadawy13',
+    code_pen: 'https://codepen.io/ABadawy',
+    code_sandbox: 'https://codesandbox.io/u/kashik0i',
+    twitter: 'https://twitter.com/_kashik0i',
+    instagram: 'https://www.instagram.com/amr_badawy000/',
+    linkedin: 'https://www.linkedin.com/in/amrabdelrahmanbadawy/',
+    facebook: 'https://web.facebook.com/amr1998amr/',
+    stack_overflow: 'https://stackoverflow.com/users/14228036/kashik0i',
+    codeforces: 'https://codeforces.com/profile/AAHB',
+    leetcode: 'https://leetcode.com/kashik0i/',
+    coursera: 'https://www.coursera.org/user/85b0045981d97cfb4cd241d993be5f76',
+    udacity: ''
+  },
+  work: {
+    pylon: 'https://pylonump.com/'
+  },
+  portfolio: {
+    kayan_academy: 'https://kayan.com',
+    peer_tutor: 'https://peertutor.xyz'
+  },
+  personal_projects: {
+    spank_that_stonk: 'https://spankthatstonk.xyz',
+    code_colab: 'https://codecolab.dev',
+    colab_hub: 'https://hub.codecolab.dev'
+  }
+}
